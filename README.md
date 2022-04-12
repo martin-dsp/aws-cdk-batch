76ce0c56596483ae122470ff73e78ffc1a9b2884
martin-dsp <kcs19542001@hotmail.com>
2022-04-12 21:10:05 +0900

    Feat using existing VPC, ECS Cluster and making taskDefinition and container to it

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-# AWS Batch
+# AWS Batch for now not to forget what should be optional
+
 blah blah --profile martin
 
-# Welcome to your CDK TypeScript project
+# What must be considered
 
-This is a blank project for TypeScript development with CDK.
-
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-## Useful commands
-
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- `테스트 가능` 어떻게 테스트 환경을 구분하게 할 것인가??
+- `템플릿` 사람들이 특정 파일에 소스코드만 넣으면 동작하도록 하려면 모든 매핑을 다 해놔야한다. -> constant name까지 주석으로 다 처리하여 가이드라인을 줘야할 듯
+- `Multiple language compliance` 어떤 언어로 짜던, packagae를 설치할 수 있어야 한다. python -> requirements.txt, node -> npm install 등..
+- `공통 모듈 삽입` Slack 등 -> 다른 언어들은 모듈 import를 어떻게 할 것인가?
+- `환경변수` 공통 사항들 (DB, AWS configs etc)
+- `config 파일은 어떻게 관리할 것인가?` 특정 환경변수 -> 주석은 얼마나 어떻게 달것인가..
+- `Deploy 전략` github repo를 자동생성하여 배포하면 GitAction에서 cdk deploy를 하여 모든 게 동작! (취소하고 싶을 떈????)
+- `Git Commands` to make repo and push
+- `Secret Keys` Git Action 쓸 때 -> ECR에 어떻게 올리지? 필요한 정보는 어떻게 저장하지.. repo 만들 때 적용시킬 수 있을까? 그러면 커멘드를 따로 치지 않으면 또 저장해놔야 할텐데..
+- ``
+- ``
+- ``
--- lib/aws-cdk-batch-stack.ts
+++ lib/aws-cdk-batch-stack.ts
@@ -1,15 +1,42 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as batch from "@aws-cdk/aws-batch";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as config from "../config.json";
 
 export class AwsCdkBatchStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 기존 VPC 활용
+    const vpc = ec2.Vpc.fromLookup(this, "getExistingVPC", {
+      vpcName: config.vpcName,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkBatchQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ECS Cluster 생성
+    const cluster = ecs.Cluster.fromClusterArn(
+      this,
+      "getExistingCluster",
+      config.clusterArn
+    );
+
+    // Task definition
+    const fargateTaskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      "makeNewTaskDefinition",
+      {
+        memoryLimitMiB: 512,
+        cpu: 256,
+      }
+    );
+
+    // Add containers to a task definition
+    const container = fargateTaskDefinition.addContainer("AddContainer", {
+      containerName: "martin",
+      image: ecs.ContainerImage.fromEcrRepository({
+        // TODO: 야 이거 왜 안되는거야...
+        repositoryUri: config.EcrRepositoryUri,
+      }),
+    });
   }
 }
