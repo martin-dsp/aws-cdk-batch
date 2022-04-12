bcf9fe1f6b5247039eec1aad0c9c73542a5f9ec7
martin-dsp <kcs19542001@hotmail.com>
2022-04-12 12:12:59 +0900

    Initial commit

---
+++ test/aws-cdk-batch.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AwsCdkBatch from '../lib/aws-cdk-batch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-batch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkBatch.AwsCdkBatchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
